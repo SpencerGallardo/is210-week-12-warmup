0cd55513861bf744b8207455eaeabc1f167f2c2a
Spencer Gallardo <s.gallardo@criteo.com>
2016-11-08 22:10:48 -0500

    Week 12 Warm up

--- task_02.py
+++ task_02.py
@@ -2,13 +2,18 @@
 # -*- coding: utf-8 -*-
 """Task 02 module"""
 
-
 import datetime
 
 
 class InvalidAgeError(Exception):
+    """ Age class."""
     pass
 
+
 def get_age(birthyear):
+    """An age calculator."""
     age = datetime.datetime.now().year - birthyear
-    return age
+    if birthyear > datetime.datetime.now().year:
+        raise InvalidAgeError
+    else:
+        return age
--- task_03.py
+++ task_03.py
@@ -1,31 +1,46 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 03 module"""
+"""Task 03 week 12"""
 
 
 import time
 
 
 class CustomLogger(object):
+    """A File Logger"""
 
     def __init__(self, logfilename):
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """Logs a message"""
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """Removes Messages"""
         handled = []
 
-        fhandler = open(self.logfilename, 'a')
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except IOError:
+            self.log('Unable to open logfile.')
+            raise IOError
         for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
+            try:
+                fhandler.write(str(entry) + '\n')
+                handled.append(index)
+            except IOError:
+                self.log('unable to write logfile')
+                handled.append('Error')
+                break
 
         fhandler.close()
 
         for index in handled[::-1]:
-            del self.msgs[index]
+            if handled[::-1] is 'Error':
+                pass
+            else:
+                del self.msgs[index]
